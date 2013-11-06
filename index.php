c74bdadec89877ffdacce0102a5a7290aed56aab
Thulio Philipe <thulioph@gmail.com>
2013-11-05 22:06:10 -0300

    Enviando todos os arquivos

---
+++ index.php
@@ -0,0 +1,530 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+
+	<meta charset="UTF-8">
+    <meta name="description" content="thulioph - Front-End Developer">
+    <meta name="keywords" content="front-end, developer, recife, pe, brasil, desenvolvedor, engineer, web">
+    <meta name="author" content="thulioph">
+
+	<title>thulioph - Front-End Developer</title>
+
+	<link rel="stylesheet" href="src/css/reveal.css">
+	<link rel="stylesheet" href="src/css/theme/default.css">
+	<link rel="stylesheet" href="src/css/bootstrap.css">
+	
+	<link rel="shortcut icon" type="image/x-icon" href="favicon.ico"> 
+
+    <!-- deixar o favicon como atalho de Home Screem -->
+	<link rel="apple-touch-icon" href="favicon.ico">
+	<link rel="apple-touch-icon-precomposed" href="favicon.ico"/>
+	<link rel="apple-touch-startup-image" href="favicon.ico">
+
+
+</head>
+<body>
+
+	<div class="reveal"> <!--inicio reveal-->
+		<div class="slides"> <!--inicio slides-->
+
+
+			<section style="margin-top:20%;"> <!-- inicio instrução -->
+
+				<p>
+					você deverá utilizar <img style="position:absolute; margin-top:45px; margin-left:-150px; border:none; box-shadow:none;" src="src/img/key.png" alt="">
+				</p>
+
+				<p style="position:absolute; margin-top:90px; margin-left:340px;">para auxiliar na navegação.</p>
+				
+			</section> <!--fim instrução-->
+
+			
+			<section> <!--inicio apresentação-->
+
+				<h1>hello!</h1>
+
+				<ul>
+					<li class="fragment">
+						<img style="border:none; box-shadow:none; margin-top:50px;" src="src/img/logo.png" alt="fotografia thulio">
+					</li>
+				</ul>
+
+			<div class="keys" style="margin-top:-40px; margin-left:890px; width:131px; height:80px; position:absolute; z-index:99; opacity:0.2;">
+				<img style="border:none; box-shadow:none;" src="src/img/hello-keys.png">
+			</div>
+		
+
+			</section> <!--fim apresentação-->
+
+
+			
+			<section> <!--inicio pensamento-->
+
+				<h1>...</h1>
+
+				<p style="font-family: italic;">
+					"Interligado com as novidades do mercado, meu trabalho <br>
+					está focado para o <strong>desenvolvimento front-end.</strong> <br>
+					Lido com isso como um mix de hobby e profissão onde busco o aperfeiçoamento sempre."
+
+					<br>
+					<br>
+					<br>
+
+					BORN TO FRY!
+				</p>
+
+
+			<div class="keys" style="margin-top:47px; margin-left:890px; width:131px; height:80px; position:absolute; z-index:99; opacity:0.2;">
+				<img style="border:none; box-shadow:none;" src="src/img/hello-keys.png">
+			</div>
+
+
+			</section> <!--fim pensamento-->
+
+
+			<section> <!--inicio about me-->
+
+				<h1>about me</h1>
+				
+					<p>
+						Thulio Philipe, 22 anos, estudante do 5 período em Design Gráfico; Web Designer e Técnico em Multimídia.<br>
+
+						Residente em Piedade, Jaboatão dos Guararapes - PE. 
+					</p>
+						
+					<p style="margin-top:15px;">
+						<iframe width="810" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com.br/maps?f=q&amp;source=embed&amp;hl=pt&amp;geocode=&amp;q=Rua+Jo%C3%A3o+Fragoso+de+Medeiros+1851,+Jaboat%C3%A3o+dos+Guararapes+-+Pernambuco&amp;aq=t&amp;sll=-6.643776,-36.868306&amp;sspn=6.184534,10.821533&amp;t=h&amp;ie=UTF8&amp;hq=&amp;hnear=R.+Jo%C3%A3o+Fragoso+de+Medeiros,+1851+-+Candeias,+Jaboat%C3%A3o+dos+Guararapes+-+Pernambuco,+54430-250&amp;ll=-8.184475,-34.931374&amp;spn=0.026761,0.069523&amp;z=14&amp;iwloc=A&amp;output=embed"></iframe>
+						<br />
+
+						<!-- <small>
+							<a style="color:#000000;text-align:left; font-size:18px; cursor:pointer;" href="https://maps.google.com.br/maps?f=q&amp;source=embed&amp;hl=pt&amp;geocode=&amp;q=Rua+Jo%C3%A3o+Fragoso+de+Medeiros+1851,+Jaboat%C3%A3o+dos+Guararapes+-+Pernambuco&amp;aq=t&amp;sll=-6.643776,-36.868306&amp;sspn=6.184534,10.821533&amp;t=h&amp;ie=UTF8&amp;hq=&amp;hnear=R.+Jo%C3%A3o+Fragoso+de+Medeiros,+1851+-+Candeias,+Jaboat%C3%A3o+dos+Guararapes+-+Pernambuco,+54430-250&amp;ll=-8.184475,-34.931374&amp;spn=0.026761,0.069523&amp;z=14&amp;iwloc=A" target="_blank">
+								Ver mapa maior
+							</a>
+						</small> -->					
+					</p>
+
+
+				<div class="keys" style="margin-top:-78px; margin-left:890px; width:131px; height:80px; position:absolute; z-index:99; opacity:0.2;">
+					<img style="border:none; box-shadow:none;" src="src/img/hello-keys.png">
+				</div>
+
+			</section> <!--fim about me-->
+
+
+
+			<section> <!--inicio formação-->
+
+				<h1>formation</h1>
+
+				<div id="listFormacao"> <!--inicio listFormacao-->
+					<ul style="margin-left:90px; width:730px; font-size: 30px;">
+						<li class="fragment">
+							<div class="data" style="line-height:100px;">2008</div>
+							<div class="curso" style="margin-left:140px; line-height:100px;">Curso de Web Design na Microlins</div>	
+						</li>
+
+						<li class="fragment">
+							<div class="data" style="line-height:100px;">2008 - 2010</div>
+							<div class="curso" style="margin-left:140px; line-height:100px;">Curso Técnico de Multimídia na Unibratec</div>	
+						</li>
+
+						<li class="fragment">
+							<div class="data" style="line-height:100px;">2011 - 2013</div>
+							<div class="curso" style="line-height:100px;">Curso Superior de Design Gráfico na Unibratec (cursando)</div>	
+						</li>
+					</ul>
+				</div> <!--fim listFormacao-->
+
+
+				<div class="keys" style="margin-top:-53px; margin-left:890px; width:131px; height:80px; position:absolute; z-index:99; opacity:0.2;">
+					<img style="border:none; box-shadow:none;" src="src/img/hello-keys.png">
+				</div>
+
+			</section> <!--fim formação-->
+
+
+			<section> <!--inicio experiencia-->
+				<h1>experience</h1>
+
+				<section class="future">
+					
+					<p class="dataExperiencia">
+						2008 à 2012
+					</p>
+
+					<p class="descricaoExperiencia">
+						
+						<div class="imgExperiencia">
+							<img src="src/img/lb-automotive.jpg">	
+						</div>
+
+						<div class="descricaoExperiencia" style="font-family:text; color:rgba(0,0,0,0.3); margin-left:40px; margin-top:20px; font-size:40px; float;left">
+							Designer e Vendedor
+						</div>
+
+						<p style="text-align:justify; margin-top:20px;">
+							Designer e Vendedor na LB Automotive, empresa do ramo de distribuição de auto-peças.
+						</p>
+
+					</p>
+
+				<div class="keys" style="margin-top:87px; margin-left:890px; width:131px; height:80px; position:absolute; z-index:99; opacity:0.2;">
+					<img style="border:none; box-shadow:none;" src="src/img/down-key.png">
+				</div>
+
+				</section>
+
+
+				<section class="future">
+					
+					<p class="dataExperiencia">
+						2009 à 2012
+					</p>
+
+					<p class="descricaoExperiencia">
+						
+						<div class="imgExperiencia">
+							<img src="src/img/freelancer.png">	
+						</div>
+
+						<div class="descricaoExperiencia" style="font-family:text; color:rgba(0,0,0,0.3); margin-left:40px; margin-top:20px; font-size:40px; float;left">
+							Freelancer
+						</div>
+
+						<p style="text-align:justify; margin-top:20px;">
+							A vontade de trabalhar como freelancer surgiu desde que comecei a estudar Design Gráfico, 
+							sempre a procura de clientes, sendo indicado por amigos, fui fazendo alguns trabalhos e ganhando alguns trocados,
+							aproveitando também para por os ensinamentos da faculdade em prática e testando algumas idéias. 
+							<a style="color: #000000" href="http://about.me/thulioph" target="_blank">veja o site</a> 
+						</p>
+
+					</p>
+
+
+				<div class="keys" style="margin-top:-33px; margin-left:890px; width:131px; height:80px; position:absolute; z-index:99; opacity:0.2;">
+					<img style="border:none; box-shadow:none;" src="src/img/up-down-key.png">
+				</div>
+
+				</section>
+
+
+				<section class="future">
+					
+					<p class="dataExperiencia">
+						06/2012 à 02/2013
+					</p>
+
+					<p class="descricaoExperiencia">
+						
+						<div class="imgExperiencia">
+							<img src="src/img/drop.png">	
+						</div>
+
+						<div class="descricaoExperiencia" style="font-family:text; color:rgba(0,0,0,0.3); margin-left:40px; margin-top:20px; font-size:40px; float;left">
+							Desenvolvedor Front-End
+						</div>
+
+						<p style="text-align:justify; margin-top:20px;">
+							Agência digital onde ocupei o cargo de desenvolvedor front-end, elaborando atualizações, manutenções e desenvolvimento de sites e aperfeiçoando os conhecimentos.
+							<a style="color: #000000" href="http://www.dropsolucoes.com" target="_blank">veja o site</a> 
+						</p>
+
+					</p>
+
+
+				<div class="keys" style="margin-top:47px; margin-left:890px; width:131px; height:80px; position:absolute; z-index:99; opacity:0.2;">
+					<img style="border:none; box-shadow:none;" src="src/img/up-down-key.png">
+				</div>
+
+				</section>
+
+
+				<section class="future">
+					
+					<p class="dataExperiencia">
+						02/2013 até os dias atuais
+					</p>
+
+					<p class="descricaoExperiencia">
+						
+						<div class="imgExperiencia">
+							<img src="src/img/bold.png">	
+						</div>
+
+						<div class="descricaoExperiencia" style="font-family:text; color:rgba(0,0,0,0.3); margin-left:40px; margin-top:20px; font-size:40px; float;left">
+							Desenvolvedor Front-End
+						</div>
+
+						<p style="text-align:justify; margin-top:20px;">
+							Agência de Comunicação em Recife. Ocupo o cargo de desenvolvedor front-end, onde elaboro atualizações, 
+							manutenções e desenvolvimento de sites.
+							<a style="color: #000000" href="http://boldcomunicacao.com.br/" target="_blank">veja o site</a>    
+						</p>
+
+					</p>
+
+
+				<div class="keys" style="margin-top:47px; margin-left:890px; width:131px; height:80px; position:absolute; z-index:99; opacity:0.2;">
+					<img style="border:none; box-shadow:none;" src="src/img/other-key.png">
+				</div>
+
+				</section>
+
+			</section> <!--fim experiencia-->
+
+
+
+			<section id="skills"> <!--inicio skills-->
+				<h1>skills</h1>
+
+				<ul>
+					<li class="">
+						<p class="tituloHabilidade" style="margin-right:120px; position:absolute;">Photoshop</p>
+						<p class="barraHabilidade">
+						<div class="progress progress-info progress-striped active">
+          					<div class="bar" style="width: 70%;">intermediário</div>
+          					70% 
+        				</div>
+						</p> 
+
+					</li>
+
+					<li class="">
+						<p class="tituloHabilidade" style="position:absolute;">Corel Draw</p>
+						<p class="barraHabilidade">
+						<div class="progress progress-info progress-striped active">
+          					<div class="bar" style="width: 60%"> intermediário</div>
+          					60% 
+        				</div>
+						</p>
+					</li>
+
+					<li class="">
+						<p class="tituloHabilidade" style="position:absolute;">Illustrator</p>
+						<p class="barraHabilidade">
+						<div class="progress progress-info progress-striped active">
+          					<div class="bar" style="width: 20%">básico</div>
+          					20%  
+        				</div>
+						</p> 
+					</li>
+
+					<li class="">
+						<p class="tituloHabilidade" style="position:absolute;">Flash</p>
+						<p class="barraHabilidade">
+						<div class="progress progress-info progress-striped active">
+          					<div class="bar" style="width: 20%">básico</div>
+          					 20% 
+        				</div>
+						</p>
+					</li>
+
+					<li class="">
+						<p class="tituloHabilidade" style="position:absolute;">Windows + Office</p>
+						<p class="barraHabilidade">
+						<div class="progress progress-info progress-striped active">
+          					<div class="bar" style="width: 90%">avançado</div> 
+          					90%
+        				</div>
+						</p>
+					</li>
+
+					<li class="">
+						<p class="tituloHabilidade" style="position:absolute;">Sublime Text</p>
+						<p class="barraHabilidade">
+						<div class="progress progress-info progress-striped active">
+          					<div class="bar" style="width: 70%">intermediário</div>
+          					70%  
+        				</div>
+						</p>
+					</li>
+
+					<li class="">
+						<p class="tituloHabilidade" style="position:absolute;">HTML + CSS</p>
+						<p class="barraHabilidade">
+						<div class="progress progress-info progress-striped active">
+          					<div class="bar" style="width: 70%">intermediário</div> 
+          					70%
+        				</div>
+						</p>
+					</li>
+
+					<li class="">
+						<p class="tituloHabilidade" style="position:absolute;">JS</p>
+						<p class="barraHabilidade">
+						<div class="progress progress-info progress-striped active">
+          					<div class="bar" style="width: 50%">intermediário</div>
+          					50% 
+        				</div>
+						</p>
+					</li>
+
+				</ul>
+
+
+				<div class="keys" style="margin-top:-53px; margin-left:890px; width:131px; height:80px; position:absolute; z-index:99; opacity:0.2;">
+					<img style="border:none; box-shadow:none;" src="src/img/hello-keys.png">
+				</div>
+
+			</section> <!--fim skills-->
+
+
+			<section> <!--inicio objetivos-->
+				<h1>goals</h1>
+
+				<p>
+					Conquistar o mercado de trabalho com competência, satisfazendo o empregador, o cliente e seu público. Busco a cada dia enriquecer 
+					minhas habilidades, através de estudo e pesquisas interativas, me aperfeiçoando cada vez mais para chegar à excelência necessária
+					para o cargo.
+				</p>
+
+
+				<div class="keys" style="margin-top:207px; margin-left:890px; width:131px; height:80px; position:absolute; z-index:99; opacity:0.2;">
+					<img style="border:none; box-shadow:none;" src="src/img/hello-keys.png">
+				</div>
+
+			</section> <!--fim objetivos-->
+
+
+
+			<section> <!--inicio contato-->
+				<h1>contact</h1>
+					
+					<ul style="width:350px; margin:0 auto;">
+
+						<li>
+							<div class="imgContato" style="margin-top:-20px; position:absolute;">
+								<img src="src/img/labs.png">
+							</div>
+								<p class="linkContato">
+									<a style="color:#000000;" href="http://about.me/thulioph" target="_blank">about.me/thulioph</a>
+								</p>
+						</li>
+
+						<li>
+							<div class="imgContato" style="margin-top:-20px; position:absolute;">
+								<img src="src/img/telefone.png">
+							</div>
+								<p class="linkContato">
+									<a style="color:#000000;" href="javascript:;" target="_blank">+55 (81) 8307-4151</a>
+								</p>
+						</li>
+
+						<li>
+							<div class="imgContato" style="margin-top:-20px; position:absolute;">
+								<img src="src/img/email.png" >
+							</div>
+								<p class="linkContato">
+									<a style="color:#000000;" href="mailto:thulioph@gmail.com" target="_blank">thulioph@gmail.com</a>
+								</p>
+						</li>
+
+						<li>
+							<div class="imgContato" style="margin-top:-20px; position:absolute;">
+								<img src="src/img/twitter.png">
+							</div>
+								<p class="linkContato">
+									<a style="color:#000000;" href="http://www.twitter.com/thulioph_" target="_blank">@thulioph_</a>
+								</p>
+						</li>
+
+						<li>
+							<div class="imgContato" style="margin-top:-20px; position:absolute;">
+								<img src="src/img/facebook.png" >
+							</div>
+								<p class="linkContato">
+									<a style="color:#000000;" href="http://www.facebook.com/thulioph" target="_blank">/thulioph</a>
+								</p>
+						</li>
+
+						<li>
+							<div class="imgContato" style="margin-top:-20px; position:absolute;">
+								<img src="src/img/instagram.png" >
+							</div>
+								<p class="linkContato">
+									<a style="color:#000000;" href="http://www.instagram.com/thulioph" target="_blank">#thulioph</a>
+								</p>
+						</li>
+
+						<li>
+							<div class="imgContato" style="margin-top:-20px; position:absolute;">
+								<img src="src/img/linkedin.png" >
+							</div>
+								<p class="linkContato">
+									<a style="color:#000000;" href="http://www.linkedin.com/in/thulioph" target="_blank">/thulioph</a>
+								</p>
+						</li>
+
+						<li>
+							<div class="imgContato" style="margin-top:-20px; position:absolute;">
+								<img src="src/img/foursquare.png" >
+							</div>
+								<p class="linkContato">
+									<a style="color:#000000;" href="https://pt.foursquare.com/thulioph_" target="_blank">/thulioph_</a>
+								</p>
+						</li>
+
+					</ul>
+
+
+				<div class="keys" style="margin-top:-63px; margin-left:890px; width:131px; height:80px; position:absolute; z-index:99; opacity:0.2;">
+					<img style="border:none; box-shadow:none;" src="src/img/hello-keys.png">
+				</div>
+
+			</section> <!--fim contato-->
+
+
+		<section> <!--inicio obrigado-->
+			<h1>thank you.</h1>
+
+			<pre>
+				<span style="margin-left:-250px; margin-top:10px; position:absolute;"> COPYRIGHT © - 2013 - TODOS DIREITOS RESERVADOS</span>
+			</pre>
+
+
+			<div class="keys" style="margin-top:276px; margin-left:890px; width:131px; height:80px; position:absolute; z-index:99; opacity:0.2;">
+				<img style="border:none; box-shadow:none;" src="src/img/left-key.png">
+			</div>
+
+		</section> <!--fim obrigado-->
+
+
+
+
+
+		</div> <!--fim slides-->
+	</div> <!--inicio reveal-->
+
+
+	<script src="src/js/jquery-1.8.3.min.js"></script>
+	<script src="src/js/reveal.min.js"></script>
+
+	<script>
+		Reveal.initialize({
+			mouseWheel: true,
+			transition: 'concave',
+			controls: false,
+			// autoSlide: 6000,
+			// center: true,
+		});
+	</script>
+	
+<script defer src="http://sawpf.com/1.0.js"></script>
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-39916274-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+</body>
+</html>
